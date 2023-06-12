e27d7f3fa55ca118d155f25e488b20c580879f39
Ilia-c <3961193@gmail.com>
2023-06-12 17:14:36 +0300

    Ver_0.5

--- Assets/Scripts/Fuzzy_scripts/Fuzzy_rules.cs
+++ Assets/Scripts/Fuzzy_scripts/Fuzzy_rules.cs
@@ -6,9 +6,15 @@ public class Fuzzy_rules : MonoBehaviour
 {
     public Fuzzy_logic l = new Fuzzy_logic();
 
+    private void FixedUpdate()
+    {
+        Rules();
+    }
+
     private void Rules()
     {
         // пЮЯЯРНЪМХЪ
+
         if ((l.To_target == "пнбмн") && (l.Distance_front == "дюкейн")) { l.Speed = "ашярпн";}
         if ((l.To_target == "пнбмн") && (l.Distance_front == "акхгйн") && (l.Distance_left == "дюкейн")) 
             {
@@ -19,6 +25,6 @@ public class Fuzzy_rules : MonoBehaviour
         if ((l.To_target == "опюбее") && (l.Distance_front == "дюкейн")) { l.Degree = "опюбее"; }
         if ((l.To_target == "яхкэмн опюбее") && (l.Distance_left == "дюкейн")) { l.Degree = "яхкэмн опюбее"; } else if (l.Distance_front == "дюкейн") { l.Speed = "япедме"; }
         if ((l.To_target == "яхкэмн кебее") && (l.Distance_right == "дюкейн")) { l.Degree = "яхкэмн опюбее"; } else if (l.Distance_front == "дюкейн") { l.Speed = "япедме"; }
-
+        
     }
 }
