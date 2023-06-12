757625cbf9f12c60f955846d6e63cc8a79be29ea
Ilia-c <3961193@gmail.com>
2023-06-12 16:28:22 +0300

    Ver_0.1

---
+++ Assets/Fuzzy_rules.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuzzy_rules : MonoBehaviour
+{
+    public Fuzzy_logic l = new Fuzzy_logic();
+
+    private void Rules()
+    {
+        // Расстояния
+        //if ((l.Distance_left == "БЛИЗКО") && ()) { l.Speed = "БЫСТРО" }
+
+    }
+}
